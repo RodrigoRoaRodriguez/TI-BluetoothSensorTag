ed46b7d3e10edbd852714f34a068b006cf9e4ffd
RodrigoRoaRodriguez <rodrigoroarod@gmail.com>
2015-02-22 01:19:20 +0100

    First upload. Able to fetch devices, services and date. By using unique IDs (UUID) for every sensor in the tag it is possible to get a list of every device that supports that service. By using the id from a device it is possible to get the Generic Attribute Profile(GATT) characteristics, which can then read as an array of Bytes.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
+using Windows.Storage.Streams;
+
+namespace UUID_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintUUIDs();
+
+            Console.WriteLine("Accelerometer:");
+            const string XYZ = "x : {0,-4} y : {1,-4} z : {1,-4}";
+
+            //Do this many(=50) times so that people can see change
+            for (int i = 0; i < 50; i++)
+            {
+                //I have managed to connect to a device and read data.
+                //These are the three bytes from the accelerometer
+                byte[] b = ReadData(Sensor.Accelerometer).Result;
+
+                //This writes it out in a human friendly format
+                Console.WriteLine(String.Format(XYZ, b[0], b[1], b[2]));
+            }
+
+            Console.ReadLine();//This is only so that terminal doesn't close immediately
+        }
+
+        /// <summary>
+        /// This will print all the sensor UUID that I have implemented
+        /// </summary>
+        static void PrintUUIDs()
+        {
+            const string FORMAT = "{0,-20}{1}";
+
+            Console.WriteLine(String.Format(FORMAT,"SENSOR NAME:", "UUID"));
+            foreach (Sensor item in Enum.GetValues(typeof(Sensor)))
+            {
+                Console.WriteLine(String.Format(FORMAT, item.ToString(), item.GetServiceUUID()));
+            }
+        }
+
+
+
+        static async Task<byte[]> ReadData(Sensor sensor)
+        {
+            byte[] invalid = { 13 };
+
+            //Get a query for devices with this service
+            string deviceSelector = GattDeviceService.GetDeviceSelectorFromUuid(new Guid(sensor.GetServiceUUID())); 
+
+            //seek devices using the query
+            var deviceCollection = await DeviceInformation.FindAllAsync(deviceSelector); 
+
+            //return info for the first device you find
+            DeviceInformation device = deviceCollection.FirstOrDefault();
+
+            if (device == null)
+                throw new Exception("Device not found.");
+
+            // using the id get the service
+            GattDeviceService service = await GattDeviceService.FromIdAsync(device.Id);
+
+            //get all characteristics
+            IReadOnlyList<GattCharacteristic> characteristics = service.GetCharacteristics(new Guid(sensor.GetDataUUID()));
+
+            if (characteristics.Count == 0)
+                throw new Exception("characteristic not found.");
+
+            //Read value of first characteristic
+            GattCharacteristic gattCharacteristic = characteristics[0];
+
+            GattReadResult read = await gattCharacteristic.ReadValueAsync(Windows.Devices.Bluetooth.BluetoothCacheMode.Uncached);
+
+            if (read.Status == GattCommunicationStatus.Unreachable)
+                throw new Exception("Device unreachable");
+
+            //Get the result into a byte array
+            Byte[] data = new byte[read.Value.Length];
+            DataReader.FromBuffer(read.Value).ReadBytes(data);
+
+            //Finished!
+            return data;
+        }
+
+    }
+}
---
+++ ConsoleApplication1/Sensor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UUID_Test
+{
+    public enum Sensor
+    {
+        // Format: Name = UUID - TI_BASE_UUID 
+        Accelerometer = 0xaa10,
+        Gyroscope = 0xAA50,
+        Humidity= 0xAA20,
+        Keys = 0xFFE0, 
+        Magnetometer = 0xAA30,
+        Pressure = 0xAA40,
+        Temperature = 0xAA00 ,
+        TestService = 0xAA60
+        // UUID for Over-The-Air-Dowload firmware service was intentionally ommited so that no one accidentally bricks a tag.
+    }
+
+    static class SensorsExtensions
+    {
+        /// <summary>
+        /// The common part that all sensor UUIDs share.
+        /// The x:s are placeholders to be replaced.
+        /// IMPORTANT: CASE SENSITIVE for some reason. Leads to unhandled exception otherwise.
+        /// </summary>
+        private const String TI_BASE_UUID = "f000XXXX-0451-4000-b000-000000000000";
+
+        public static String GetServiceUUID(this Sensor sensor)
+        {
+            return TI_BASE_UUID.Replace("XXXX", ((int)sensor).ToString("x4"));
+        }
+
+        public static String GetDataUUID(this Sensor sensor)
+        {
+            return TI_BASE_UUID.Replace("XXXX", ((int)sensor+1).ToString("x4"));
+        }
+    }
+}
